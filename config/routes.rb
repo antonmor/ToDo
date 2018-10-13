47fa2bd4b54d6a51cb9985bd626defa6000bdcd2
antonmor <ingantonmor@gmail.com>
2018-10-13 13:23:38 -0500

    change view when authentications user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  authenticated :user do
+    root "todo_lists#index", as: :authenticated_root
+
+  end
+
+
   resources :todo_lists do
     resources :todo_items do
       member do
@@ -8,6 +15,6 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- # root "todo_lists#index"
+  #root "todo_lists#index"
   root "static_pages#home"
 end
