a027df0940adeac8c84a683f2a9dbfdfa379c525
antonmor <ingantonmor@gmail.com>
2018-10-13 13:36:11 -0500

    concern devise_whitlist

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,11 @@
+module DeviseWhiteList
+extend ActiveSupport::Concern
+    include do 
+     before_action :configure_permitted_parameters, if: :devise_controller?
+    end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sing_up, keys: [:name],[:lastname],[:city],[:country],[:address],[:about],[:address],[:gender],[:picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name],[:lastname],[:city],[:country],[:address],[:about],[:address],[:gender],[:picture])
+  end
+end
