e8359cf85fcfa5b5c5da1a8d114a3b69e437465a
antonmor <ingantonmor@gmail.com>
2018-10-16 14:44:04 -0500

    Active Storage configurado y puesto en marcha

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,6 +1,5 @@
 class TodoListsController < ApplicationController
   before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
-
   # GET /todo_lists
   # GET /todo_lists.json
   def index
