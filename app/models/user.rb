173e59accb6cd236d6d6b682193cafd9894b945e
antonmor <ingantonmor@gmail.com>
2018-10-19 22:57:07 -0500

    Ya funciona todo, solo que no he podido arreglar el git

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :todo_list
+  has_many :todo_list #para que funcione bien todo_list, no debe llevar 's' al final
   has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
